a679f96e8d17f650eb6900c7303f9645ce394302
Daniel Tartaglia <danielt1263@gmail.com>
2022-03-16 20:42:06 -0400

    Less code means fewer bugs. Also, Subjects provide a convenient way to poke around Rx, however they are not recommended for day to day use. Instead of using subjects, favor the factory methods.

--- RxCellError/ViewModel.swift
+++ RxCellError/ViewModel.swift
@@ -11,44 +11,19 @@ import RxSwift
 import RxCocoa
 
 enum ItemModel: IdentifiableType, Equatable {
-  typealias Identity = String
-  
-  var identity: String {
+  var identity: Int {
     switch self {
     case .timer(let cellViewModel):
-      return "\(cellViewModel.id)"
+      return cellViewModel.id
     }
   }
   
   case timer(CellViewModel)
-  
-  static func == (lhs: Self, rhs: Self) -> Bool {
-    return lhs.identity == rhs.identity
-  }
 }
 
-struct SectionModel: AnimatableSectionModelType {
-  var title: String
-  var items: [Item]
-  
-  var identity: String { title }
-  
-  typealias Item = ItemModel
-  
-  init(original: Self, items: [Item]) {
-    self = original
-    self.items = items
-  }
-  
-  init(title: String, items: [Item]) {
-    self.title = title
-    self.items = items
-  }
-}
+typealias SectionModel = AnimatableSectionModel<String, ItemModel>
 
 class ViewModel: NSObject {
-  var disposeBag = DisposeBag()
-  
   struct Input { }
   
   struct Output {
@@ -59,24 +34,21 @@ class ViewModel: NSObject {
 
   func transform(from input: Input) -> Output {
 
-    let sections = self.sections.asDriver(onErrorJustReturn: [])
-      .startWith([])
-    
     let initialState = State<Int>(items: [])
     
     let itemsMainSource = Observable.just([1,2])
       .observe(on: MainScheduler.instance)
-      .flatMapLatest { Observable.from($0)}
+      .flatMapLatest { Observable.from($0) }
 
     let anotherSource: Observable<Int> = .empty()
     
     let items = Observable.merge(itemsMainSource, anotherSource)
-      .scan(initialState) { state, item in
-        return state.execute(newItem: item)
+      .scan(into: initialState) { state, item in
+        state.update(newItem: item)
       }
       .map { $0.items }
-      
-    items
+
+    let sections = items
       .observe(on: MainScheduler.asyncInstance)
       .flatMapLatest { items -> Observable<[ItemModel]> in
         let itemModels = items.map { item in
@@ -85,11 +57,11 @@ class ViewModel: NSObject {
         return .just(itemModels)
       }
       .map {
-        [SectionModel(title: "title", items: $0)]
+        [SectionModel(model: "title", items: $0)]
       }
-      .bind(to: self.sections)
-      .disposed(by: disposeBag)
-        
+      .startWith([])
+      .asDriver(onErrorJustReturn: [])
+
     return Output(sections: sections)
   }
 }
